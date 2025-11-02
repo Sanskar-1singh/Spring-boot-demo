4c39741843f8b15266538d721cc3d67d1673a789
Sanskar-1singh <sanskarsingh812@gmail.com>
2025-11-02 17:31:06 +0530

    02

--- src/main/java/com/example/TodoApiSpring/TodoController.java
+++ src/main/java/com/example/TodoApiSpring/TodoController.java
@@ -2,15 +2,18 @@ package com.example.TodoApiSpring;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @Deprecated
+ * @Deprecated only warns developers, it doesn’t deactivate the code.
+ */
+
 @RestController
+@RequestMapping("/api/v1/todos")
 public class TodoController {
 
     private static List<Todo> todos;
@@ -21,7 +24,7 @@ public class TodoController {
         todos.add(new Todo(12, true, "running"));
     }
 
-    @GetMapping("/todos")
+    @GetMapping("/")
     public List<Todo> getTodos() {
         return todos;
     }
@@ -32,7 +35,17 @@ public class TodoController {
         /**
          * we can use this annottaions to set the status code @ResponseStatus(Httpstatus.CREATED)
          */
-        return ResponseEntity.status(HttpStatus.CREATED).body(newTodo);;
+        return ResponseEntity.status(HttpStatus.CREATED).body(newTodo);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Todo> getTodoById(@PathVariable int id) {
+        for(Todo t : todos) {
+            if(t.getId() == id) {
+                return ResponseEntity.ok().body(t);
+            }
+        }
+        return ResponseEntity.notFound().build();
     }
 
 }
